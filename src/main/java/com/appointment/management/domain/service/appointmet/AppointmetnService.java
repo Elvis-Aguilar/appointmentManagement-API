d433bcf6aa8e6d8ec19167ff94ff5f5d2880b557
Elvis Aguilar <elvisaguilar201930304@cunoc.edu.gt>
2024-10-26 01:06:54 -0600

    add envio de facturacion a cliente

--- src/main/java/com/appointment/management/domain/service/appointmet/AppointmetnService.java
+++ src/main/java/com/appointment/management/domain/service/appointmet/AppointmetnService.java
@@ -1,14 +1,31 @@
 package com.appointment.management.domain.service.appointmet;
 
+import com.appointment.management.application.exception.RequestConflictException;
 import com.appointment.management.domain.dto.appoinment.AppointmentDto;
+import com.appointment.management.domain.dto.business.BusinessConfigurationDto;
+import com.appointment.management.domain.dto.business.ServiceDto;
+import com.appointment.management.domain.dto.user.UserDto;
+import com.appointment.management.domain.service.UserService;
+import com.appointment.management.domain.service.auth.EmailService;
+import com.appointment.management.domain.service.auth.TemplateRendererService;
+import com.appointment.management.domain.service.business.BusinessConfigurationService;
+import com.appointment.management.domain.service.business.ServiceService;
 import com.appointment.management.persistance.entity.AppointmentEntity;
+import com.appointment.management.persistance.entity.BusinessConfigurationEntity;
+import com.appointment.management.persistance.entity.ServiceEntity;
+import com.appointment.management.persistance.entity.UserEntity;
+import com.appointment.management.persistance.enums.StatusAppointment;
 import com.appointment.management.persistance.repository.AppointmentRepository;
 import com.appointment.management.presentation.mapper.business.AppointmentMapper;
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -21,6 +38,20 @@ public class AppointmetnService {
     @Autowired
     private AppointmentMapper appointmentMapper;
 
+    @Autowired
+    private BusinessConfigurationService businessConfigurationService;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private TemplateRendererService templateRendererService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ServiceService serviceService;
 
     @Transactional(readOnly = true)
     public List<AppointmentDto> getAllAppointments() {
@@ -70,5 +101,46 @@ public class AppointmetnService {
             throw new IllegalArgumentException("Appointment not found");
         }
         appointmentRepository.deleteById(id);
+    }
+
+    public AppointmentDto completedAppointment(Long id) {
+        AppointmentEntity existingAppointment = appointmentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Appointment not found"));
+        existingAppointment.setStatus(StatusAppointment.COMPLETED);
+        AppointmentEntity updatedEntity = appointmentRepository.save(existingAppointment);
+
+        /*logica de envio de factura*/
+        BusinessConfigurationDto busines = this.businessConfigurationService.findFirst();
+        LocalDate date = LocalDate.now();
+        UserDto user = userService.findUserById(existingAppointment.getCustomer().getId()).orElseThrow();
+        ServiceDto service = this.serviceService.getServiceById(existingAppointment.getService().getId());
+        LocalDate dateService = existingAppointment.getStartDate().toLocalDate();
+
+        BigDecimal price = service.price();
+        BigDecimal additionalAmount = new BigDecimal("15.00");
+
+        BigDecimal newPrice = price.add(additionalAmount);
+        Map<String, Object> templateVariables = Map.of(
+                "company", busines,
+                "order_date",date,
+                "client", user,
+                "servicio", service.name(),
+                "fecha",dateService,
+                "price", service.price(),
+                "priceTotal",newPrice
+        );
+
+        String confirmationHtml = templateRendererService.renderTemplate("bill", templateVariables);
+
+        try {
+            emailService.sendHtmlEmail("Appointment-Management", user.email(),
+                    "Facturacion ", confirmationHtml);
+        } catch (MessagingException e) {
+            System.out.println(e.getMessage());
+            throw new RequestConflictException("No se pudo enviar el correo de confirmacion "+e.getMessage());
+        }
+
+        return appointmentMapper.toDto(updatedEntity);
+
     }
 }
--- src/main/java/com/appointment/management/presentation/controller/AppointmentController.java
+++ src/main/java/com/appointment/management/presentation/controller/AppointmentController.java
@@ -1,6 +1,7 @@
 package com.appointment.management.presentation.controller;
 
 import com.appointment.management.domain.dto.appoinment.AppointmentDto;
+import com.appointment.management.domain.dto.business.ServiceDto;
 import com.appointment.management.domain.service.appointmet.AppointmetnService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -43,6 +44,12 @@ public class AppointmentController {
                                                             @RequestBody @Valid AppointmentDto appointmentDto) {
         AppointmentDto updatedAppointment = appointmentService.updateAppointment(id, appointmentDto);
         return ResponseEntity.ok(updatedAppointment);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<AppointmentDto> updateServiceStatus(@PathVariable Long id, @RequestBody String status) {
+        AppointmentDto result = this.appointmentService.completedAppointment(id);
+        return ResponseEntity.ok(result);
     }
 
     @DeleteMapping("/{id}")
